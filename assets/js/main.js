8b5afe9d7f81753121092b93c1fc8da6be74ad90
Yanabet <yanasomok@gmail.com>
2025-09-29 18:31:37 +0300

    lets talk btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,7 +123,6 @@ const sections = document.querySelectorAll('.fade');
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
-    console.log(entry);
     
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
